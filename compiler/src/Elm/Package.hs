86bf7517ef6a98aef9283f47765fe72ad6acea4b
Anupam Jain <ajnsit@gmail.com>
2020-04-13 10:00:31 +0000

    FFI is usable by all packages. No difference between kernel and user packages.

--- compiler/src/Elm/Package.hs
+++ compiler/src/Elm/Package.hs
@@ -81,8 +81,9 @@ data Canonical =
 
 
 isKernel :: Name -> Bool
-isKernel (Name author _) =
-  author == elm || author == elm_explorations
+isKernel (Name _author _) =
+  -- author == elm || author == elm_explorations
+  True
 
 
 toChars :: Name -> String
